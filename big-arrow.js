2eaedf02b733258ab8f89649266c76c04cc39b8a
unknown <islam15-3879@diu.edu.bd>
2022-02-19 01:05:01 +0600

    added

---
+++ big-arrow.js
@@ -0,0 +1,25 @@
+const add = (num1,num2)=>num1+num2;
+const result =add(45,34);
+console.log(result);
+
+const multiply =(num1,num2,num3)=>num1*num2*num3;
+const result2 =multiply(2,34,4);
+
+
+const fiveTimes =(num)=>num*10;
+const output =fiveTimes(7);
+
+
+const getName =()=>'jahid';
+const name =getName();
+
+
+const doMath =(x,y)=>{
+    const sum =x+y;
+
+    const diffe=x-y;
+    const output =result *5;
+    return output;
+}
+
+const total =doMath(34,4);
