8cfb2dc5f7eb704313bf32d8ab46393f2c5f372c
unknown <islam15-3879@diu.edu.bd>
2022-02-18 20:48:08 +0600

    added

---
+++ basic.js
@@ -0,0 +1,25 @@
+// variable declare 
+var deposit =400;
+
+
+if(deposit> 500){
+
+
+}
+if(deposit<200){
+
+
+}
+if(deposit ==500){
+
+}
+if(deposit !=500){
+
+}
+if(deposit<=500){
+
+}
+if(deposit>=500){
+    
+}
+
