95485870490db4f57697030b0b9976d13e4c4302
unknown <islam15-3879@diu.edu.bd>
2022-02-18 20:57:38 +0600

    added

--- basic.js
+++ basic.js
@@ -6,20 +6,55 @@ if(deposit> 500){
 
 
 }
-if(deposit<200){
+else if(deposit<200){
 
 
 }
-if(deposit ==500){
+else if(deposit ==500){
 
 }
-if(deposit !=500){
+else if(deposit !=500){
 
 }
-if(deposit<=500){
+else if(deposit<=500){
 
 }
-if(deposit>=500){
-    
+else if(deposit>=500){
+
+}
+else{
+    console.log('how funny');
+}
+
+//
+const number =[34,56,43,65,33,56,54,32,56];
+const numberCount=number.length(number);
+number.push(111);
+number.pop();
+number[2]=34;
+// cheack weatehr included in the array 
+if(number.indexOf(222)!=-1){
+
+};
+
+// loop
+// while loop for loop
+for(numbers of number){
+
+}
+
+// function
+function getFulName(first,second){
+    const name =first+''+second;
+
+}
+
+const person= getFulName("rahim",'ali');
+
+// Object
+const bottle ={
+    color:'yellow',
+    contain: 'water',
+    price :'234'
 }
 
