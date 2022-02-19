368b7d215957f410f00dda6bb7498a16a1140336
unknown <islam15-3879@diu.edu.bd>
2022-02-19 14:56:11 +0600

    added

---
+++ spread.js
@@ -0,0 +1,13 @@
+const numbers =[ 45,67,45,67,65];
+// console.log(numbers);
+// console.log(...numbers);
+const max =Math.E(23,99,65,9,76);
+const maxInArray =Math.max(...numbers);
+console.log(max);
+numbers.push(45);
+console.log(numbers);
+
+// refference or pointer is here 
+const number2 =numbers;
+numbers.push(55);
+console.log(number2);
